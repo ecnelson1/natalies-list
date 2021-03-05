f8851eee79cb90d3c60d257814316d7d8242312e
Eion Nelson <ecnelson1@live.com>
2021-03-05 06:03:02 -0800

    got booklist page rendered with search function

---
+++ src/Home/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class HomePage extends Component {
+    render() {
+        return ( 
+            <div>
+            <h1>Welcome to Natalies List!</h1>
+            <p>
+                Click on Sign Up or Login above to build your Reading list!  
+            </p>
+            </div>
+        )
+    }}
